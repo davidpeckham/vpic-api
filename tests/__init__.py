98a32ecf51132fc0d11622b7ba3f4c9670eb73f0
David Peckham <dave.peckham@icloud.com>
2021-05-07 11:34:56 -0400

    removed patching code

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,7 @@
 import json
 
-# from vpic.client_base import standardize_variable_names
-
 
 def expected_result(response_path):
     with response_path.open() as fp:
         content = json.load(fp)
-    # TODO Update JSON response files to reflect the actual expected
-    # result and then remove this call to standardize_variable_names
-    # return standardize_variable_names(content["expected_result"])
     return content["expected_result"]
