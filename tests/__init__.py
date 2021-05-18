5a2d31a202077507bb11a5b54542ed39765c3ac1
David Peckham <dave.peckham@icloud.com>
2021-05-17 21:41:50 -0400

    Use UTF-8 explicity for Windows

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,6 @@ import json
 
 
 def expected_result(response_path):
-    with response_path.open() as fp:
+    with response_path.open(encoding="utf-8") as fp:
         content = json.load(fp)
     return content["expected_result"]
